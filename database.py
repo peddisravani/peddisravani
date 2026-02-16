164052e0c559263493e43e4f870a1b9afc563ad4
peddisravani <91547998+peddisravani@users.noreply.github.com>
2026-02-16 10:17:40 -0500

    Delete database.py

--- database.py
+++
@@ -1,3 +0,0 @@
-PRODUCTS=[{"id":1,"title":"Red Running Shoes","category":"shoes"},
-{"id":2,"title":"Blue Sports Shoes","category":"shoes"},
-{"id":3,"title":"Smart Watch","category":"electronics"}]
